c4cb87bc097f573daf4213af456af142ad4c15d0
lordwartz <Lwartz@yandex.ru>
2024-11-10 00:53:42 +0500

    Add helmets to pages

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,6 +1,7 @@
-import Main from '../../pages/main/main.tsx';
+import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AppRoute, AuthorizationStatus } from '../../const.ts';
+import Main from '../../pages/main/main.tsx';
 import Login from '../../pages/login/login.tsx';
 import Offer from '../../pages/offer/offer.tsx';
 import NotFound from '../../pages/not-found/not-found.tsx';
@@ -13,23 +14,25 @@ type AppProps = {
 
 export default function App({ placesCount }: AppProps) {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Main placesCount={placesCount} />} />
-        <Route path={AppRoute.Login} element={<Login />} />
-        <Route
-          path={AppRoute.Favorites}
-          element={
-            <PrivateRoute
-              authorizationStatus={AuthorizationStatus.Unauthorized}
-            >
-              <Favorites />
-            </PrivateRoute>
-          }
-        />
-        <Route path={AppRoute.Offer} element={<Offer />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Main placesCount={placesCount} />} />
+          <Route path={AppRoute.Login} element={<Login />} />
+          <Route
+            path={AppRoute.Favorites}
+            element={
+              <PrivateRoute
+                authorizationStatus={AuthorizationStatus.Unauthorized}
+              >
+                <Favorites />
+              </PrivateRoute>
+            }
+          />
+          <Route path={AppRoute.Offer} element={<Offer />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 
 export default function NotFound() {
   //todo. Проработать страницу
   return (
     <>
+      <Helmet>
+        <title>6 cities. Page not found</title>
+      </Helmet>
       <h1>Not Found. 404</h1>
       <Link className="not-found__main" to="/">
         На главную
