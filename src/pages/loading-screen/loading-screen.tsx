d87b097f5158674a751c0f85b60628c4ceeee62e
lordwartz <Lwartz@yandex.ru>
2025-01-13 07:10:57 +0500

    Пофиксил микро баг в лоадере

--- src/pages/loading-screen/loading-screen.tsx
+++ src/pages/loading-screen/loading-screen.tsx
@@ -1,12 +1,5 @@
-import Header from '../../components/header/header.tsx';
-import { useAppSelector } from '../../hooks';
-
 export default function LoadingScreen() {
-  const user = useAppSelector((state) => state.user);
   return (
-    <>
-      <Header user={user} />
-      <p>Loading ...</p>
-    </>
+    <p>Loading ...</p>
   );
 }
